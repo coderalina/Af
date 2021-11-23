1bdaacf0e1b3c3e658269177f8e2f70f80f754b1
coderalina <75560766+coderalina@users.noreply.github.com>
2021-11-24 01:53:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,26 @@
 function setDate(){ document.getElementById('date').innerHTML = new Date().toDateString();
 }
+
+function bird(){
+
+    window.location="index.html"
+
+}
+
+function aquarium(){
+
+    window.location="aquarium.html"
+
+}
+
+function dog(){
+
+    window.location="dog.html"
+
+}
+
+function cat(){
+
+    window.location="cat.html"
+
+}
